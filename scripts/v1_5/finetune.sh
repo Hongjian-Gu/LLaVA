04bdc8ed90e8a99c4e927c9fe30cf1d00807a8f6
Hongjian <hongjian.gu@huawei.comscreen>
2024-11-21 15:22:12 -0500

    adaptive fix

--- scripts/v1_5/finetune.sh
+++ scripts/v1_5/finetune.sh
@@ -2,22 +2,22 @@
 
 deepspeed llava/train/train_mem.py \
     --deepspeed ./scripts/zero3.json \
-    --model_name_or_path lmsys/vicuna-13b-v1.5 \
+    --model_name_or_path /home/hongjian-g/Workspace/rag_planning_scene_graph_with_llava/llms/llava-v1.5-7b \
     --version v1 \
-    --data_path ./playground/data/llava_v1_5_mix665k.json \
-    --image_folder ./playground/data \
+    --data_path /home/hongjian-g/Workspace/rag_planning_scene_graph_with_llava/playground/data/LLaVA-Instruct-150K \
+    --image_folder /home/hongjian-g/Workspace/rag_planning_scene_graph_with_llava/playground/data \
     --vision_tower openai/clip-vit-large-patch14-336 \
-    --pretrain_mm_mlp_adapter ./checkpoints/llava-v1.5-13b-pretrain/mm_projector.bin \
+    --pretrain_mm_mlp_adapter ./checkpoints/llava-v1.5-7b-pretrain/mm_projector.bin \
     --mm_projector_type mlp2x_gelu \
     --mm_vision_select_layer -2 \
     --mm_use_im_start_end False \
     --mm_use_im_patch_token False \
     --image_aspect_ratio pad \
     --group_by_modality_length True \
-    --bf16 True \
-    --output_dir ./checkpoints/llava-v1.5-13b \
+    --bf16 False \
+    --output_dir ./checkpoints/llava-v1.5-7b \
     --num_train_epochs 1 \
-    --per_device_train_batch_size 16 \
+    --per_device_train_batch_size 4 \
     --per_device_eval_batch_size 4 \
     --gradient_accumulation_steps 1 \
     --evaluation_strategy "no" \
@@ -29,9 +29,8 @@ deepspeed llava/train/train_mem.py \
     --warmup_ratio 0.03 \
     --lr_scheduler_type "cosine" \
     --logging_steps 1 \
-    --tf32 True \
+    --tf32 False \
     --model_max_length 2048 \
     --gradient_checkpointing True \
     --dataloader_num_workers 4 \
     --lazy_preprocess True \
-    --report_to wandb
--- scripts/v1_5/pretrain.sh
+++ scripts/v1_5/pretrain.sh
@@ -2,20 +2,20 @@
 
 deepspeed llava/train/train_mem.py \
     --deepspeed ./scripts/zero2.json \
-    --model_name_or_path lmsys/vicuna-13b-v1.5 \
+    --model_name_or_path /home/hongjian-g/Workspace/rag_planning_scene_graph_with_llava/llms/llava-v1.5-7b \
     --version plain \
-    --data_path ./playground/data/LLaVA-Pretrain/blip_laion_cc_sbu_558k.json \
-    --image_folder ./playground/data/LLaVA-Pretrain/images \
+    --data_path /home/hongjian-g/Workspace/rag_planning_scene_graph_with_llava/playground/data/LLaVA-Pretrain/blip_laion_cc_sbu_558k.json\
+    --image_folder /home/hongjian-g/Workspace/rag_planning_scene_graph_with_llava/playground/data/LLaVA-Pretrain/images \
     --vision_tower openai/clip-vit-large-patch14-336 \
     --mm_projector_type mlp2x_gelu \
     --tune_mm_mlp_adapter True \
     --mm_vision_select_layer -2 \
     --mm_use_im_start_end False \
     --mm_use_im_patch_token False \
-    --bf16 True \
-    --output_dir ./checkpoints/llava-v1.5-13b-pretrain \
+    --bf16 False \
+    --output_dir ./checkpoints/llava-v1.5-7b-pretrain \
     --num_train_epochs 1 \
-    --per_device_train_batch_size 32 \
+    --per_device_train_batch_size 4 \
     --per_device_eval_batch_size 4 \
     --gradient_accumulation_steps 1 \
     --evaluation_strategy "no" \
@@ -27,9 +27,8 @@ deepspeed llava/train/train_mem.py \
     --warmup_ratio 0.03 \
     --lr_scheduler_type "cosine" \
     --logging_steps 1 \
-    --tf32 True \
+    --tf32 False \
     --model_max_length 2048 \
     --gradient_checkpointing True \
     --dataloader_num_workers 4 \
     --lazy_preprocess True \
-    --report_to wandb
